ea526479bfd941ae1eab0652c30d19d3e44419ff
Gaurav Dangi <gauravdangi96@gmail.com>
2017-06-17 16:44:37 +0530

    Binary Tree Operations

---
+++ Binary_Search_Tree_Operations/BinarySearchTree.java
@@ -0,0 +1,201 @@
+package binary.search.tree;
+import java.util.Scanner;
+/*
+ * @author Gaurav
+ */
+public class BinarySearchTree {
+Node root=null;  // root node
+// ------------------------- add Node method ---------------------------------------
+    public void addNode(int a){
+  
+    Node newNode = new Node(a);
+    if(root==null){
+     root = newNode;
+    }
+    else{
+      Node temp = root;
+      Node temp2 = root;
+      while(true){
+     if(a<=temp.value){
+      temp = temp.left;
+      if(temp==null){
+       temp2.left = newNode;
+       return;
+      }
+     }
+     else{
+     temp = temp.right;
+     if(temp==null){
+       temp2.right = newNode;
+       return;
+      }
+     }
+     temp2 = temp;
+    }
+    }
+    
+    }
+    
+    //if node has two child Or degree of node is 2
+    boolean full(Node a){
+    if(a.left != null && a.right!=null)
+        return true;
+    else
+        return false;
+    }
+    
+    public Node findSmallest(Node a){
+    Node temp = a.right;
+    if(isLeaf(temp)) return temp;
+    Node parent = temp;
+    while(true){
+    temp = temp.left;
+    if(temp.left == null){
+     parent.left = temp.right;
+     return temp;
+    }
+    parent = temp;
+    }
+    
+    }
+    
+    public boolean isLeaf(Node a){
+     if(a.left == null && a.right == null) return true;
+     else return false;
+    }
+    
+    // ---------------------- Delete Node --------------------------------------
+    public void deleteNode(int a){
+    
+    if(a==root.value){
+        if(isLeaf(root)){
+         root = null;
+         return;
+        }
+     Node smallest = findSmallest(root);
+     root.value = smallest.value;
+     return;
+    }
+     
+     Node temp = root;
+     Node parent = root;
+    
+    while(true && !isLeaf(parent)){
+        
+        //  ----  temp.value > a -----
+     if(temp.value>a){
+      temp = temp.left;
+      if(temp.value == a){
+          // if temp is a leaf node
+        if(isLeaf(temp)){
+          parent.left = null;
+          return;
+        }
+        // if temp has two child
+        else if(full(temp)){
+         Node smallest = findSmallest(temp);
+         temp.value = smallest.value;
+         return;
+        }
+        else if(temp.right!=null){
+         parent.left = temp.right;
+         return;
+        }
+        else{
+         parent.left = temp.left;
+         return;
+        }
+      }
+     }
+     
+     // ----- temp.value < a -------
+     else{
+     temp = temp.right;
+     if(temp.value == a){
+          // if temp is a leaf node
+        if(isLeaf(temp)){
+          parent.right = null;
+          return;
+        }
+        // if temp has two child
+        else if(full(temp)){
+         Node smallest = findSmallest(temp);
+         temp.value = smallest.value;
+         return;
+        }
+        else if(temp.right!=null){
+         parent.right = temp.right;
+         return;
+        }
+        else{
+         parent.right = temp.left;
+         return;
+        }
+      }
+     
+     }
+     
+     parent = temp;
+    }
+    System.out.println("\n"+a+" is not present in the list");
+    }
+   
+    // --------------------- Traversing methods -----------------------------------
+    public void inOrder(Node r){
+    if(r == null)
+        return;
+    inOrder(r.left);
+    System.out.print(" "+r.value);
+    inOrder(r.right);
+    }
+    
+    public void postOrder(Node r){
+    if(r==null)
+        return;
+    postOrder(r.left);
+    postOrder(r.right);
+    System.out.print(" "+r.value);
+    }
+    
+    public void preOrder(Node r){
+    if(r==null)
+        return;
+    System.out.print(" "+r.value);
+    postOrder(r.left);
+    postOrder(r.right);
+    
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.addNode(3);
+        tree.addNode(4);
+        tree.addNode(5);
+        tree.addNode(1);
+        tree.addNode(8);
+        tree.addNode(2);
+        tree.addNode(7);
+        
+        System.out.print("\nIn-order traversing:");
+        tree.inOrder(tree.root);
+        System.out.print("\nPre-order traversing:");
+        tree.preOrder(tree.root);
+        System.out.print("\nPost-order traversing:");
+        tree.postOrder(tree.root);
+        System.out.print("\nEnter node you want to delete: ");
+        int n = scan.nextInt();
+        tree.deleteNode(n);
+        System.out.print("\nIn-order traversing:");
+        tree.inOrder(tree.root);
+        System.out.print("\nPre-order traversing:");
+        tree.preOrder(tree.root);
+        System.out.print("\nPost-order traversing:");
+        tree.postOrder(tree.root);
+        
+        System.out.println();
+        System.exit(0);
+    }
+    
+}
+
---
+++ Binary_Search_Tree_Operations/Node.java
@@ -0,0 +1,15 @@
+
+package binary.search.tree;
+
+/*
+ * @author Gaurav
+ */
+public class Node {
+    int value;
+    Node left;
+    Node right;
+    Node(int value){
+     this.value = value;
+     
+    }
+}
